4e24910ee08ff133e513706ad8052c0037a8a597
Thomas Maltesen <thma@sund.ku.dk>
2018-02-02 15:32:03 +0100

    Update NoMissObs.R

--- R/NoMissObs.R
+++ R/NoMissObs.R
@@ -1,8 +1,33 @@
-NoMissObs<-function(data, output, var.list, global=TRUE, ...){
-if(global){
-  } else{}
+RemoveMissingObervation<-function(data, output, var.list, global=TRUE, ...){
+  NameOfData<-deparse(substitute(data))
+  if(is.data.frame(data)){
+    if(is.logical(global)){
+      if(global){
+        if(missing(output)){
+          NewName<-paste0(NameOfData,"Redu")
+        } else {
+          NewName<-paste0(NameOfData,output)
+        }
+        message(paste("Name of dataset is:",NewName))
+      }
+  
+      if(missing(var.list)){
+        TempData<-na.omit(data)
+      } else {
+        TempData<-data[eval(parse(text=paste0("!is.na(data$",var.list,")",collapse = " & "))),])
+      }
+      message(paste(NameOfData,"is reduced from",nrow(data),"to",nrow(TempData)))
 
-  if(missing(output)){
-    deparse(substitute(data))
+      if(global){
+        assign(NewName,TempData,envi=.GlobalEnv)
+        message(paste("You have a global dataset and name is:",NewName))
+      } else {
+        return(TempData)
+      }
+    } else {
+      message(paste(NameOfData,"is not a data.frame")))
+    }
+  } else {
+    message(paste(NameOfData,"is not a data.frame")))
   }
 }
