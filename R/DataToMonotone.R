34ef4579ce199a30fa468bd87759f3d968122edf
Thomas Maltesen <thma@sund.ku.dk>
2018-05-24 14:59:15 +0200

    Update DataToMonotone.R

--- R/DataToMonotone.R
+++ R/DataToMonotone.R
@@ -9,16 +9,9 @@ DataToMonotone<-function(data, orderSeq, ...){
   data2<-data1[eval(parse(text=paste0("data1$",orderSeq[ma],"_R==1"))),]
   data2$MONOTONE<-NA
   
-  for(i in 2:ma){
-    for(j in 1:nrow(data2)){
-      eval(parse(text=
-                   paste0("data2$MONOTONE[j]<-ifelse(all(c(",paste0(
-                     "data2$",orderSeq[c(ma:i)],"_R[j]==1",collapse = ","),")) & (all(c(",
-                     paste0("data2$",orderSeq[c((i-1):1)],collapse = "_R[j]==0,"),"_R[j]==0))|all(c(",
-                     paste0("data2$",orderSeq[c((i-1):1)],collapse = "_R[j]==1,"),"_R[j]==1))), TRUE , FALSE)")
-      ))
-    }}
-
+ for(j in 1:nrow(data2)){
+   data2$MONOTONE[j]<-
+   all(sapply(1:(ma-1),function(i)eval(parse(text=paste0("data2$",orderSeq[i],"_R[j]<=","data2$",orderSeq[i+1],"_R[j]")))))
+    }
   return(data2)
-  
 }
