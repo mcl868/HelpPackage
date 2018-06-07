a29225a55f32c7fe01da43a332c1255466684803
Thomas Maltesen <thma@sund.ku.dk>
2018-06-07 14:23:36 +0200

    Update DataToMonotone.R

--- R/DataToMonotone.R
+++ R/DataToMonotone.R
@@ -1,6 +1,6 @@
 DataToMonotone<-function(data, orderSeq, redu, ...){
   result<-list()
-  
+
   data1<-data[,orderSeq]
   ma<-length(orderSeq)
   for(i in 1:ma){
@@ -8,14 +8,14 @@ DataToMonotone<-function(data, orderSeq, redu, ...){
       eval(parse(text=paste0("data1$",orderSeq[i],"_R[j]<-1*(!is.na(","data1$",orderSeq[i],"[j]))")))
     }}
   
-  data2<-data1[eval(parse(text=paste0("data1$",orderSeq[ma],"_R==1"))),]
+  data2<-data1[eval(parse(text=paste0("data1$",orderSeq[1],"_R==1"))),]
   data2$C<-rowSums(data2[,c(paste0(orderSeq,"_R"))])
   data2$C[data2$C==ma]<-Inf
   data2$MONOTONE<-NA
   
   for(j in 1:nrow(data2)){
     data2$MONOTONE[j]<-
-      all(sapply(1:(ma-1),function(i)eval(parse(text=paste0("data2$",orderSeq[i],"_R[j]<=","data2$",orderSeq[i+1],"_R[j]")))))
+      all(sapply(1:(ma-1),function(i)eval(parse(text=paste0("data2$",orderSeq[i],"_R[j]>=","data2$",orderSeq[i+1],"_R[j]")))))
   }
   
   if(!missing(redu)){
