fe23745b9bae52b4bfda32d40ed78f50bbde1da7
Thomas Maltesen <thma@sund.ku.dk>
2018-06-07 14:46:41 +0200

    Update print.DataToMonotoneMissing.R

--- R/print.DataToMonotoneMissing.R
+++ R/print.DataToMonotoneMissing.R
@@ -1,4 +1,9 @@
 print.DataToMonotoneMissing<-function(x){
   cat("head:\n")
-  print(head(x$data))
+  cat("Reduced: ",x$reduObj,".\n")
+  if(x$reduObj){
+    cat("Montone:\n")
+    print(table(x$C))
+  }
+  print(table(x$C))
 }
