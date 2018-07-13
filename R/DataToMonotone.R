fbcdf8f18247218a0b4c614724f4b927443cfc2e
Thomas Maltesen <thma@sund.ku.dk>
2018-07-13 13:34:29 +0200

    Update DataToMonotone.R

--- R/DataToMonotone.R
+++ R/DataToMonotone.R
@@ -3,17 +3,19 @@ DataToMonotone<-function(response, covariates, missing, data, redu, ...){
   data1<-data[, c(covariates, response)]
   if(length(response)>1){
     DataSetformat<-"Longitudinal"
-  } else {
-    DataSetformat<-"OneResponse"
-  }
-  ma<-length(missing)
-  for(i in 1:ma){
-    for(j in 1:nrow(data1)){
-      eval(parse(text = paste0("data1$", missing[i], "_R[j]<-1*(!is.na(","data1$", missing[i], "[j]))")))
+    ma<-length(missing)
+    for(i in 1:ma){
+      for(j in 1:nrow(data1)){
+        eval(parse(text = paste0("data1$", missing[i], "_R[j]<-1*(!is.na(","data1$", missing[i], "[j]))")))
+      }
     }
+    data2<-data1[eval(parse(text = paste0("data1$", missing[1],"_R==1"))), ]
+  } else {
+    ma<-1
+    DataSetformat<-"OneResponse"
+    data2<-data1
   }
 
-  data2<-data1[eval(parse(text = paste0("data1$", missing[1],"_R==1"))), ]
   
   data2$C<-rowSums(data2[, c(paste0(missing, "_R"))])
   data2$C[data2$C==ma]<-Inf
