d7b46ba2dc0981ad0a0744f48bdbadbd1be74cf4
Thomas Maltesen <thma@sund.ku.dk>
2018-10-22 15:44:04 +0100

    Update missingpattern.R

--- R/missingpattern.R
+++ R/missingpattern.R
@@ -31,8 +31,8 @@ missing.pattern<-function(response, covariates, data, pattern, ...){
   result$covariatesObj<-covariates
   result$pattern<-pattern
   result$responseObj<-response
-  result$count<-table(data$C)
-  result$percent<-table(data$C)/nrow(data)
+  result$count<-table(data2$C)
+  result$percent<-table(data2$C)/nrow(data2)
 
   attr(result, "class")<-"DataToPattern"
   out<-structure(result, class = "DataToPattern")
