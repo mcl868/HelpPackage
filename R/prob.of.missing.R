915083390dfb15b247dd0e12c5b5f31b931dd82a
Thomas Maltesen <thma@sund.ku.dk>
2019-01-22 14:50:42 +0100

    Update prob function

--- R/prob.of.missing.R
+++ R/prob.of.missing.R
@@ -47,13 +47,27 @@ prob.of.missing<-function(object, regression, list.out = TRUE, completecase = FA
   	  if(missing(regression)){
       message("regression is simple")
       }
+      regList<-vector("list",length(covariatesObj))
+      CoefList<-vector("list",length(covariatesObj))
   	  if(is.null(order))order<-covariatesObj
   	  levels<-as.numeric(rownames(table(objdata$C))[!rownames(table(objdata$C)) %in% Inf])
   	  for(iii_ in levels){
   	    lambdamodel<-paste0("1*(C==",iii_,") ~ ",paste0(order[1:iii_],collapse=" + "))
-  	    lambda<-predict(glm(lambdamodel, data= objdata[objdata$C>=iii_,],family=binomial()),type="response", newdata=objdata)
+            regList[[iii_]]<-lambdamodel
+            CoefList[[iii_]]<-coef(glm(lambdamodel, data= objdata[objdata$C>=iii_,],family=binomial()))
+            lambda<-predict(glm(lambdamodel, data= objdata[objdata$C>=iii_,],family=binomial()),type="response", newdata=objdata)
   	    eval(parse(text=paste0("objdata$lambda",iii_,"<-lambda")))
-  	  }
+          }
+          qq<-c(1:length(covariatesObj))
+          eval(parse(text=paste0("objdata$lambda",qq[!(qq %in% levels)],"<-0")))
+          objdatalambda<-objdata[,paste0("lambda",qq)]
+          objdatalambda[is.na(objdata[,covariatesObj])]<-NA
+          eval(parse(text=paste0("objdata$lambda",qq,"<-NULL")))
+          objdata<-cbind(objdata,objdatalambda)
+          objdata$K0<-1
+          for(iiii_ in qq){eval(parse(text=paste0("objdata$K",iiii_,"<-objdata$K",iiii_-1,"*(1-objdata$lambda",iiii_,")")))}
+          eval(parse(text=paste0("objdata$varpi<-objdata$K",length(covariatesObj))))
+          objdata$K0<-NULL
     }
   }
     
@@ -68,7 +82,13 @@ prob.of.missing<-function(object, regression, list.out = TRUE, completecase = FA
     }
     out$data<-objdata
     if(patternObj=="TwoLevel"){
+      out$pattern<-patternObj
       out$varpimodel<-varpimodel
+      out$CoefList<-CoefList
+    }
+    if(patternObj=="Monotone"){
+      out$pattern<-patternObj
+      out$regList<-regList
       out$CoefList<-CoefList
     }
   } else {
