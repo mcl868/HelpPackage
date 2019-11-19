5d77593bc86cd02c4c05d82241eea2d6611ebb7b
Thomas Maltesen <atmaltesen@gmail.com>
2019-11-19 09:26:18 +0100

    Update rangeCol.R

--- R/rangeCol.R
+++ R/rangeCol.R
@@ -1,6 +1,6 @@
 rangeCol<-function(x, na.rm = FALSE){
   rancol<-matrix(NA,nrow=2,ncol=ncol(x))
-  for(i in 1:ncol(x)){rancol[,i]<-range(x, na.rm = na.rm)}
+  for(i in 1:ncol(x)){rancol[,i]<-range(x[,i], na.rm = na.rm)}
   rownames(rancol)<-c("min","max")
   return(rancol)
 }
